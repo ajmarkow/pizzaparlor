0d48c71ff8fe9e8994b0ddfcac265bd48994285d
Alexander Markow <alexanderjmarkow@gmail.com>
2020-08-28 14:36:49 -0700

    Add working function to set price based on size property of pizza object.  Add jQuery code to replace text in order summary with value of PizzaOrdered object properties.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,61 @@
-$(document).ready(function () {
-  // * Business Logic
-  function Pizza(size) {
-    this.size = size;
-    this.toppings = [];
-    this.pizzasummary = [];
-    this.cost = 0;
-    this.currentId = 0;
+function Pizza(size) {
+  this.size = size;
+  this.toppings = [];
+  this.pizzasummary = [];
+  this.cost = 0;
+  this.currentId = 0;
+}
+
+// var sizeinput = String($("#size").val());
+var toppingsselected = $(".radio:checked").val();
+var topping1 = $("input#topping1").val();
+var topping2;
+var topping3;
+var topping4;
+var topping5;
+var topping6;
+
+Pizza.prototype.assignId = function () {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+Pizza.prototype.setCost = function () {
+  if (String($("#size").val()) === "small") {
+    return this.pizzasummary.push("$18");
+  } else {
+    return this.pizzasummary.push("$28");
   }
 
-  var sizeinput = $("#size").val();
-  var toppingsselected = $(".radio:checked").val();
+  // if (sizeinput === "large") {
+  //   this.cost ="$28";
+  //   return (this.cost = "$28");
+  // } else {
+  //   this.cost.replaceWith("$18");
+  //   return (this.cost = "$18");
+  // }
+};
 
-  Pizza.prototype.assignId = function () {
-    this.currentId += 1;
-    return this.currentId;
-  };
-
-  Pizza.prototype.determineCost = function () {};
-
-  Pizza.prototype.addSelectedToppings = function () {
-    $(".radio").each(function () {
-      toppings.push($("input[type=radio]:checked").val());
-    });
-  };
-  // * Interface Logic
-
+Pizza.prototype.addSelectedToppings = function (topping) {
+  Pizza.toppings.push(topping);
+};
+// * Interface Logic
+$(document).ready(function () {
   $("form").submit(function (event) {
     event.preventDefault();
     let pizzaOrdered = new Pizza($("#size").val());
     pizzaOrdered.pizzasummary.splice(0, 1, $("#size").val());
-    pizzaOrdered.addSelectedToppings();
-    console.log(pizzaOrdered);
-    $("#outputforordersummary").replaceWith(pizzaOrdered.pizzasummary);
+    pizzaOrdered.setCost();
+    pizzaOrdered.cost = pizzaOrdered.pizzasummary.slice(1);
+    console.log(pizzaOrdered.pizzasummary);
+    $("outputforpizzacost").replaceWith(pizzaOrdered.pizzasummary.slice(1));
+    $("#outputforordersummary").replaceWith(
+      "Your pizza is a size " +
+        pizzaOrdered.size +
+        " and will cost " +
+        pizzaOrdered.cost +
+        ".  It will be topped with:" +
+        pizzaOrdered.toppings
+    );
   });
 });
